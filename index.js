e0dcc5eadfa7189926b8c6af025c72ce962a45c3
Andy Dayton <amdayton@gmail.com>
2017-04-05 09:42:12 -0400

    Added some styles

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 'use strict'
 
+const artTpl = require('./tpl/art.hbs');
+
 import 'whatwg-fetch';
-import Promise from 'promise-polyfill'; 
+import Promise from 'promise-polyfill';
+import font from 'google-fonts';
 
 const OF_PATH = 'https://api.openframe.io/v0/frames/';
 const PORTRAIT_URI = OF_PATH + '58e15977a9c1b11803b242b5';
 const LANDSCAPE_URI = OF_PATH + '58e15977a9c1b11803b242b5';
 
-let portraitArt;
-let landscapeArt;
+const wrapEl = document.createElement('div');
+const portraitEl = document.createElement('div');
+const landscapeEl = document.createElement('div');
+
+let portraitObj;
+let landscapeObj;
+
+font({
+  'Overpass': true
+});
 
 const getJson = () => {
   let portraitPromise = fetch(PORTRAIT_URI).then((resp) => resp.json() );
@@ -18,15 +29,25 @@ const getJson = () => {
 }
 
 const jsonLoaded = (json) => {
-  portraitArt = json[0].current_artwork;
-  landscapeArt = json[1].current_artwork;
+  portraitObj = json[0].current_artwork;
+  landscapeObj = json[1].current_artwork;
   draw();
 }
 
 const draw = () => {
-  console.log(portraitArt, landscapeArt);
+  portraitEl.innerHTML = artTpl(portraitObj);
+  landscapeEl.innerHTML = artTpl(landscapeObj);
   window.setTimeout(() => getJson().then(jsonLoaded), 5000);
 }
+
+wrapEl.id = "wrap";
+portraitEl.id = "portrait-art";
+landscapeEl.id = "landscape-art";
+
+// add to DOM
+wrapEl.appendChild(portraitEl);
+wrapEl.appendChild(landscapeEl);
+document.body.appendChild(wrapEl);
 
 getJson().then(jsonLoaded);
 
